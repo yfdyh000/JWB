3fcd367b67bb4ef44892f770541ff81b6d256bee
YFdyh000 <yfdyh000@gmail.com>
2020-12-12 18:09:25 +0800

    update

--- load.js
+++ load.js
@@ -1,11 +1,11 @@
-/** This script contains the code required for loading [[User:Joeytje50/JWB.js]].
+/** This script contains the code required for loading [[User:YFdyh000/JWB.js]].
  *  All other code is located at that page.
  */
 
 //Idea by [[User:Epicgenius]]
-$.when(mw.loader.using(['mediawiki.util'], $.ready)).done( function() {
-	mw.util.addPortletLink("p-tb", mw.config.get('wgArticlePath').replace('$1', "Project:AutoWikiBrowser/Script"), "JS Wiki Browser", "tb-awbscript", "Run Javascript Wiki Browser");
+$.when(mw.loader.using(['mediawiki.util'], $.ready)).done(function() {
+    mw.util.addPortletLink("p-tb", mw.config.get('wgArticlePath').replace('$1', "Project:AutoWikiBrowser/Script"), "JWB Tool", "tb-awbscript", "Run Javascript Wiki Browser");
 });
 
-if (mw.config.get('wgCanonicalNamespace')+':'+mw.config.get('wgTitle') === 'Project:AutoWikiBrowser/Script' && mw.config.get('wgAction') == 'view')
-	mw.loader.load('//zh.wikipedia.org/w/index.php?title=User:YFdyh000/JWB.js&action=raw&ctype=text/javascript');
+if (mw.config.get('wgCanonicalNamespace') + ':' + mw.config.get('wgTitle') === 'Project:AutoWikiBrowser/Script' && mw.config.get('wgAction') == 'view')
+    mw.loader.load('//zh.wikipedia.org/w/index.php?title=User:YFdyh000/JWB.js&action=raw&ctype=text/javascript');
